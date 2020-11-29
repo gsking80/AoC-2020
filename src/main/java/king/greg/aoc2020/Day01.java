1b743e0be92942d0bf90e844964c25ce0284e7b4
Greg King <greg.king@gfs.com>
2020-11-29 14:04:11 -0500

    2020 Initial Commit

--- src/main/java/king/greg/aoc2020/Day01.java
+++ src/main/java/king/greg/aoc2020/Day01.java
@@ -1,2 +1,9 @@
-package king.greg.aoc2020;public class Day01 {
+package king.greg.aoc2020;
+
+public class Day01 {
+
+  public static boolean alwaysTrue() {
+    return true;
+  }
+
 }
--- src/test/java/king/greg/aoc2020/Day01Test.java
+++ src/test/java/king/greg/aoc2020/Day01Test.java
@@ -1,4 +1,13 @@
-import static org.junit.Assert.*;
+package king.greg.aoc2020;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
 public class Day01Test {
-  
+
+  @Test
+  public void testSolution1() {
+    Assertions.assertThat(Day01.alwaysTrue()).isTrue();
+  }
+
 }
