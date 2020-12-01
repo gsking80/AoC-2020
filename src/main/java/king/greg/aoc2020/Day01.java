184858e838d05d9dd4a7efd1d486eae7a7ec6b94
gsking80 <king.greg@gmail.com>
2020-12-01 00:21:29 -0500

    Day 01

--- src/main/java/king/greg/aoc2020/Day01.java
+++ src/main/java/king/greg/aoc2020/Day01.java
@@ -1,9 +1,54 @@
 package king.greg.aoc2020;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Day01 {
 
-  public static boolean alwaysTrue() {
-    return true;
+  final List<Integer> numbers;
+
+  public Day01(FileReader fileReader) {
+    try {
+      final BufferedReader buf = new BufferedReader(fileReader);
+      numbers = new ArrayList<>();
+
+      while(true) {
+        final String lineJustFetched = buf.readLine();
+        if(null == lineJustFetched) {
+          break;
+        } else {
+          numbers.add(Integer.valueOf(lineJustFetched));
+        }
+      }
+    } catch (IOException ioe) {
+      throw new RuntimeException();
+    }
   }
 
+  public int getTwoProduct() {
+    for (int i = 0; i < numbers.size(); i++) {
+      for (int j = i + 1; j < numbers.size(); j++) {
+        if (numbers.get(i) + numbers.get(j) == 2020) {
+          return numbers.get(i) * numbers.get(j);
+        }
+      }
+    }
+    return 0;
+  }
+
+  public int getThreeProduct() {
+    for (int i = 0; i < numbers.size(); i++) {
+      for (int j = i + 1; j < numbers.size(); j++) {
+        for (int k = j+1; k < numbers.size(); k++) {
+          if (numbers.get(i) + numbers.get(j) + numbers.get(k) == 2020) {
+            return numbers.get(i) * numbers.get(j) * numbers.get(k);
+          }
+        }
+      }
+    }
+    return 0;
+  }
 }
--- src/test/java/king/greg/aoc2020/Day01Test.java
+++ src/test/java/king/greg/aoc2020/Day01Test.java
@@ -1,13 +1,28 @@
 package king.greg.aoc2020;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 public class Day01Test {
 
   @Test
-  public void testSolution1() {
-    Assertions.assertThat(Day01.alwaysTrue()).isTrue();
+  public void testSolution1() throws FileNotFoundException {
+
+    final FileReader fileReader = new FileReader(getClass().getClassLoader().getResource("Day01/input.txt").getPath());
+    final Day01 day01 = new Day01(fileReader);
+    Assertions.assertThat(day01.getTwoProduct()).isEqualTo(996996);
+
+  }
+
+  @Test
+  public void testSolution2() throws FileNotFoundException {
+
+    final FileReader fileReader = new FileReader(getClass().getClassLoader().getResource("Day01/input.txt").getPath());
+    final Day01 day01 = new Day01(fileReader);
+    Assertions.assertThat(day01.getThreeProduct()).isEqualTo(996996);
+
   }
 
 }
