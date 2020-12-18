5b2c20a2c7359c44b0e8ff170a70d70f1f4488c5
gsking80 <king.greg@gmail.com>
2020-12-18 02:24:07 -0500

    ok I lied.  Partial cleanup now.

--- src/main/java/king/greg/aoc2020/Day18.java
+++ src/main/java/king/greg/aoc2020/Day18.java
@@ -4,15 +4,30 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Day18 {
 
   final List<String> homework;
   int index = 0;
+  final boolean part2;
+
+  public Day18() {
+    this(false);
+  }
+
+  public Day18(final boolean part2) {
+    homework = Collections.emptyList();
+    this.part2 = part2;
+  }
 
   public Day18(final FileReader fileReader) {
+    this(fileReader, false);
+  }
 
+  public Day18(final FileReader fileReader, final boolean part1) {
+    this.part2 = part1;
     homework = new ArrayList<>();
     try {
       final BufferedReader buf = new BufferedReader(fileReader);
@@ -38,17 +53,8 @@ public class Day18 {
     return total;
   }
 
-  public long sumAllEquations2() {
-    long total = 0;
-    for (final String equation : homework) {
-      index = 0;
-      total += solveEquation2(equation);
-    }
-    return total;
-  }
-
-  public long solveEquation(final String equation) {
-    long answer = 0;
+  public Long solveEquation(final String equation) {
+    Long answer = 0L;
     Long currentNumber = null;
     Character currentOperand = null;
     while (index < equation.length()) {
@@ -62,48 +68,15 @@ public class Day18 {
         currentNumber += current - '0';
       } else if (current == '(') {
         currentNumber = solveEquation(equation);
-      } else if (current == '+' || current == '*') {
-        currentOperand = current;
-      } else if (current.equals(' '))  {
-        if (null == currentOperand) {
-          answer = currentNumber;
-          currentNumber = null;
-        } else if (currentNumber != null) {
-          answer = operation(currentOperand, answer, currentNumber);
-          currentNumber = null;
-          currentOperand = null;
-        }
-      }
-      else if (current.equals(')')) {
-        return operation(currentOperand, answer, currentNumber);
-      }
-    }
-
-    return operation(currentOperand, answer, currentNumber);
-  }
-
-  public long solveEquation2(final String equation) {
-    long answer = 0;
-    Long currentNumber = null;
-    Character currentOperand = null;
-    while (index < equation.length()) {
-      Character current = equation.charAt(index);
-      index++;
-      if (current >= '0' && current <= '9') {
-        if (null == currentNumber) {
-          currentNumber = 0L;
-        }
-        currentNumber *= 10;
-        currentNumber += current - '0';
-      } else if (current == '(') {
-        currentNumber = solveEquation2(equation);
       } else if (current == '+') {
         currentOperand = current;
       } else if (current == '*') {
         currentOperand = current;
-        index++;
-        currentNumber = solveEquation2(equation);
-        index--;
+        if(part2) {
+          index++;
+          currentNumber = solveEquation(equation);
+          index--;
+        }
       } else if (current.equals(' '))  {
         if (null == currentOperand) {
           answer = currentNumber;
@@ -118,7 +91,7 @@ public class Day18 {
         return operation(currentOperand, answer, currentNumber);
       }
     }
-    index++;
+    index++; // Make sure we don't go backwards here.
     return operation(currentOperand, answer, currentNumber);
   }
 
--- src/test/java/king/greg/aoc2020/Day18Test.java
+++ src/test/java/king/greg/aoc2020/Day18Test.java
@@ -8,23 +8,20 @@ import org.junit.Test;
 public class Day18Test {
 
   @Test
-  public void testEquation1() throws FileNotFoundException {
-    final FileReader fileReader = new FileReader(getClass().getClassLoader().getResource("Day18/input.txt").getPath());
-    final Day18 day18 = new Day18(fileReader);
+  public void testEquation1() {
+    final Day18 day18 = new Day18();
     Assertions.assertThat(day18.solveEquation("2 * 3 + (4 * 5)")).isEqualTo(26);
   }
 
   @Test
-  public void testEquation2() throws FileNotFoundException {
-    final FileReader fileReader = new FileReader(getClass().getClassLoader().getResource("Day18/input.txt").getPath());
-    final Day18 day18 = new Day18(fileReader);
+  public void testEquation2() {
+    final Day18 day18 = new Day18();
     Assertions.assertThat(day18.solveEquation("5 + (8 * 3 + 9 + 3 * 4 * 3)")).isEqualTo(437);
   }
 
   @Test
-  public void testEquation3() throws FileNotFoundException {
-    final FileReader fileReader = new FileReader(getClass().getClassLoader().getResource("Day18/input.txt").getPath());
-    final Day18 day18 = new Day18(fileReader);
+  public void testEquation3() {
+    final Day18 day18 = new Day18();
     Assertions.assertThat(day18.solveEquation("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")).isEqualTo(12240);
   }
 
@@ -36,38 +33,34 @@ public class Day18Test {
   }
 
   @Test
-  public void testEquation4() throws FileNotFoundException {
-    final FileReader fileReader = new FileReader(getClass().getClassLoader().getResource("Day18/input.txt").getPath());
-    final Day18 day18 = new Day18(fileReader);
-    Assertions.assertThat(day18.solveEquation2("2 * 3 + (4 * 5)")).isEqualTo(46);
+  public void testEquation4() {
+    final Day18 day18 = new Day18(true);
+    Assertions.assertThat(day18.solveEquation("2 * 3 + (4 * 5)")).isEqualTo(46);
   }
 
   @Test
-  public void testEquation5() throws FileNotFoundException {
-    final FileReader fileReader = new FileReader(getClass().getClassLoader().getResource("Day18/input.txt").getPath());
-    final Day18 day18 = new Day18(fileReader);
-    Assertions.assertThat(day18.solveEquation2("5 + (8 * 3 + 9 + 3 * 4 * 3)")).isEqualTo(1445);
+  public void testEquation5() {
+    final Day18 day18 = new Day18(true);
+    Assertions.assertThat(day18.solveEquation("5 + (8 * 3 + 9 + 3 * 4 * 3)")).isEqualTo(1445);
   }
 
   @Test
-  public void testEquation6() throws FileNotFoundException {
-    final FileReader fileReader = new FileReader(getClass().getClassLoader().getResource("Day18/input.txt").getPath());
-    final Day18 day18 = new Day18(fileReader);
-    Assertions.assertThat(day18.solveEquation2("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")).isEqualTo(669060);
+  public void testEquation6() {
+    final Day18 day18 = new Day18(true);
+    Assertions.assertThat(day18.solveEquation("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")).isEqualTo(669060);
   }
 
   @Test
-  public void testEquation7() throws FileNotFoundException {
-    final FileReader fileReader = new FileReader(getClass().getClassLoader().getResource("Day18/input.txt").getPath());
-    final Day18 day18 = new Day18(fileReader);
-    Assertions.assertThat(day18.solveEquation2("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")).isEqualTo(23340);
+  public void testEquation7() {
+    final Day18 day18 = new Day18(true);
+    Assertions.assertThat(day18.solveEquation("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")).isEqualTo(23340);
   }
 
   @Test
   public void testSolution2() throws FileNotFoundException {
     final FileReader fileReader = new FileReader(getClass().getClassLoader().getResource("Day18/input.txt").getPath());
-    final Day18 day18 = new Day18(fileReader);
-    Assertions.assertThat(day18.sumAllEquations2()).isEqualTo(224973686321527L);
+    final Day18 day18 = new Day18(fileReader, true);
+    Assertions.assertThat(day18.sumAllEquations()).isEqualTo(224973686321527L);
   }
 
 }
